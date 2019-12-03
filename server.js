9d04d7d2a248c08084175ae5bc4d435951c66740
victorgabrielms <vgabriel.vg84@gmail.com>
2019-12-03 10:06:26 -0300

    Realtime chat

--- server.js
+++ server.js
@@ -1,27 +1,30 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 
-app.use(express.static(path.resolve(__dirname, 'public')));
-app.set('views', path.resolve(__dirname, 'public'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+app.use(express.static(path.resolve(__dirname, "public")));
+app.set("views", path.resolve(__dirname, "public"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
 
-app.use('/', (req, res)=> {
-    res.render('index.html');
+app.use("/", (req, res) => {
+  res.render("index.html");
 });
 
 let messages = [];
 
-io.on('connection', socket => {
-    console.log(`Socket conectado ${socket.id}`);
+io.on("connection", socket => {
+  console.log(`Socket conectado ${socket.id}`);
 
-    socket.on('sendMessage', data => {
-        messages.push(data);
-    });
-})
+  socket.emit("previousMessages", messages);
 
-server.listen(3000)
+  socket.on("sendMessage", data => {
+    messages.push(data);
+    socket.broadcast.emit("reciveMessage", data);
+  });
+});
+
+server.listen(3000);
